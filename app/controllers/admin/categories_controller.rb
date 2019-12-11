c73e3fb6c734c1ae0a26d756cad3efd350f04622
Chris Mastel <chris.mastel@gmail.com>
2019-12-11 23:55:01 +0000

    initial setup of admin categories

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,30 @@
+class Admin::CategoriesController < ApplicationController
+
+  http_basic_authenticate_with name: ENV['USER_NAME'], password: ENV['PASSWORD']
+
+  def index
+    @categories = Category.order(id: :desc).all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @Category = Category.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, :products], notice: 'Category created!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(
+      :name,
+    )
+  end
+end
