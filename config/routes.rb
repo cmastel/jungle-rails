8ec2afc761f06bb7c8bfb250e627e35faa7bcf2d
Chris Mastel <chris.mastel@gmail.com>
2019-12-16 18:47:42 +0000

    initialize rspec

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   root to: 'products#index'
 
-  get 'about', to: 'about#show'
+  get '/about' => 'about#show'
 
   # Register routes
   get '/signup' => 'users#new'
