094eef457314d6c1d2243f5c3dd05fb501bf20a5
Chris Mastel <chris.mastel@gmail.com>
2019-12-18 21:33:20 +0000

    update Login and Signup form styling

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,4 +40,15 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 
   config.web_console.whitelisted_ips = ['10.0.2.0/24']
+
+  # config.action_mailer.raise_delivery_errors = true
+  # ActionMailer::Base.smtp_settings = {
+  #   :address        => 'smtp.gmail.com',
+  #   :domain         => 'mail.google.com',
+  #   :port           => 587,
+  #   :user_name      => 'foo@gmail.com',
+  #   :password       => '******',
+  #   :authentication => :plain,
+  #   :enable_starttls_auto => true
+  # }
 end
